62026ae6b1cd02e292fa1b168f0efa5be8780483
weewStack <weew.stack@gmail.com>
2019-10-31 15:19:04 +0900

    Add GPIO output folder

--- 010-STM32F1_OUTPUT/main.c
+++ 010-STM32F1_OUTPUT/main.c
@@ -5,12 +5,13 @@ void delay(int rep);
 
 int main(void)
 {
-	RCC->APB2ENR |= 0x10; /// 0b10000
-	GPIOC->CRH &= 0xFF0FFFFF;
-	GPIOC->CRH |= 0x00300000;
-	GPIOC->ODR  |= 0x2000;
+	RCC->APB2ENR |= 0x10; /// Or  0b10000 --> Anabling Preiph GPIOC
+	GPIOC->CRH &= 0xFF0FFFFF;  /// Reset the PORT C PIN 13
+	GPIOC->CRH |= 0x00300000;  /// Set Port C PIN 13 as Output 
+	GPIOC->ODR  |= 0x2000;     /// Set Port C Pin 13 
 	while(1)
 	{
+		/// Blinking the Port C pin 13 
 		GPIOC->ODR  |= 0x2000;
 		delay(10);
 		GPIOC->ODR  &= ~0x2000;
@@ -20,6 +21,8 @@ int main(void)
 
 }
 
+
+/// Random time delay Function
 void delay(int rep)
 {
 	for(;rep>0;rep--)
